52e10be1df7398f0470d6cfc90beb7c7744bf008
skummesalma463-pixel <skummesalma463@gmail.com>
2026-06-01 15:23:15 +0530

    Time: 3 ms (90.44%), Space: 19.3 MB (14.78%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        # total=0
+        # for i in range(1,num):
+        #     if num%i==0:
+        #         total +=i
+        # return total==num
+
+        s=1
+        if num ==1:
+            return False
+        for i in range(2,int(num**0.5)+1):
+            if num%i==0:
+                s +=(i+num//i)
+        return num==s
