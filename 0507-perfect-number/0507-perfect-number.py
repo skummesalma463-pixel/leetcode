e78ef53fe46575f087e46cf9cd12c449c822b375
skummesalma463-pixel <skummesalma463@gmail.com>
2026-06-01 15:30:56 +0530

    Time: 0 ms (100%), Space: 19.1 MB (85.52%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -6,10 +6,11 @@ class Solution:
         #         total +=i
         # return total==num
 
-        s=1
-        if num ==1:
-            return False
-        for i in range(2,int(num**0.5)+1):
-            if num%i==0:
-                s +=(i+num//i)
-        return num==s
+        # s=1
+        # if num ==1:
+        #     return False
+        # for i in range(2,int(num**0.5)+1):
+        #     if num%i==0:
+        #         s +=(i+num//i)
+        # return num==s
+        return num in{6,28,496,8128,33550336}
