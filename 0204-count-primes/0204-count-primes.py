854216a7aeffb9a0fc6d3f9ed3a816496428d603
skummesalma463-pixel <skummesalma463@gmail.com>
2026-06-02 14:29:57 +0530

    Time: 1260 ms (62.21%), Space: 57.9 MB (68.58%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -14,3 +14,16 @@ class Solution:
             p += 1
 
         return sum(prime)
+
+    
+        # count=0
+        # for num in range(2,n):
+        #     prime=True
+        #     for i in range(2,num):
+        #         if num%i==0:
+        #             prime=False
+        #             break
+
+        #     if prime:
+        #         count +=1
+        # return count
