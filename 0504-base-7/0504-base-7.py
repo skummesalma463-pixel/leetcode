9978ebf058c5e07f9be98d44e16092d9b67528fa
skummesalma463-pixel <skummesalma463@gmail.com>
2026-07-02 02:08:30 +0530

    Time: 0 ms (100%), Space: 19.4 MB (11.16%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+
+        sign = ""
+
+        if num < 0:
+            sign = "-"
+            num = -num
+
+        ans = ""
+
+        while num > 0:
+            ans = str(num % 7) + ans
+            num //= 7
+
+        return sign + ans
