f352249d619796ce838194d4564d763abefe64e2
skummesalma463-pixel <skummesalma463@gmail.com>
2026-09-23 20:13:27 +0530

    Time: 56 ms (93.41%), Space: 30.7 MB (93.25%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -5,19 +5,14 @@ class Solution:
             return -1
         if target == 0:
             return len(nums)
-
         left = 0
         curr = 0
         longest = -1
-
         for right in range(len(nums)):
             curr += nums[right]
-
             while curr > target:
                 curr -= nums[left]
                 left += 1
-
             if curr == target:
                 longest = max(longest, right - left + 1)
-
         return -1 if longest == -1 else len(nums) - longest
